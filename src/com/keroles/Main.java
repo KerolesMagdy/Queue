80469c26494890010c8d34709ae2eea659e54961
Keroles <keromagdy589@gmail.com>
2022-10-01 19:58:15 +0200

    first update

--- src/com/keroles/Main.java
+++ src/com/keroles/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
         //normal queue
+        System.out.println("Queue with array");
         MyQueue queue=new MyQueue();
         queue.enqueue(1);
         queue.enqueue(2);
@@ -13,10 +14,21 @@ public class Main {
         queue.enqueue(5);
         queue.dequeue();
         queue.dequeue();
+        queue.enqueue(6);
+        queue.enqueue(7);
+        queue.dequeue();
+        queue.dequeue();
+        queue.enqueue(8);
+        queue.enqueue(9);
+        queue.dequeue();
+        queue.dequeue();
+        queue.dequeue();
+
         queue.display();
 
 
         // Queue linked list
+        System.out.println("Queue with linked list");
         MyQueueLinkedList queueLinkedList=new MyQueueLinkedList();
         queueLinkedList.enqueue(1);
         queueLinkedList.enqueue(2);
--- src/com/keroles/MyQueue.java
+++ src/com/keroles/MyQueue.java
@@ -1,12 +1,12 @@
 package com.keroles;
 
 public class MyQueue {
-    int front,rear,size=0;
+    public int front,rear;
     int arr[];
 
     public MyQueue() {
         front=rear=-1;
-        arr=new int[10];
+        arr=new int[5];
     }
 
     public void enqueue(int data){
@@ -14,9 +14,9 @@ public class MyQueue {
             System.out.println("overflow to enqueue");
             return;
         }
-        arr[++rear]=data;
-        if (front==-1)front++;
-        size++;
+        if(front==-1)front=0;
+        rear=(rear+1)%arr.length;
+        arr[rear]=data;
     }
 
     public void dequeue(){
@@ -24,26 +24,26 @@ public class MyQueue {
             System.out.println("underflow to dequeue");
             return;
         }
-        front++;
-        if (rear==0)rear--;
-        size--;
+        if (front==rear)front=rear=-1;
+        else front=(front+1)% arr.length;
     }
 
     public void display()
     {
-        if (front==-1) {
+        if (isEmpty()) {
             System.out.println("under flow to display");
             return;
         }
-        for(int i=front;i<=rear;i++)
+        for(int i=front;i!=rear+1;i=(i+1)%arr.length)
             System.out.println(arr[i]);
     }
 
     public boolean isFull(){
-        return rear==arr.length-1? true:false;
+        return ((front == 0 && rear == arr.length - 1) ||
+                (rear == front - 1 ))? true:false;
     }
 
     public boolean isEmpty(){
-        return front==-1||front>rear? true:false;
+        return front==-1? true:false;
     }
 }
